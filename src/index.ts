868714b7b90fe1f166f34136eb6787d5d3eaf05f
Sam <54m152@gmail.com>
2022-11-30 16:30:43 +0800

    Starting with docs

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,9 @@ import recordRequests from "./record/recordRequests";
 import replayRequests from "./replay/replayRequests";
 
 export type ReplayConfig = {
-    interceptPattern: string,
+    interceptPattern?: string,
     dynamicRequestEnvComponents?: Array<string>,
+    responseDelayOverride?: number,
 };
 
 export enum ReplayMode {
@@ -11,7 +12,7 @@ export enum ReplayMode {
     Replaying
 }
 
-export default function enableCypressReplay(mode: ReplayMode | null = null) {
+export default function enableCypressReplay(mode: ReplayMode | null = null, config: ReplayConfig = {}) {
     const replayMode = mode !== null ? mode : (Cypress.env('REPLAY_RECORD_REQUESTS') ? ReplayMode.Recording : ReplayMode.Replaying);
 
     if (replayMode === ReplayMode.Recording) {
