cd027c98668020e0a9f260f08ee4b81a79367b18
Sam <54m152@gmail.com>
2022-12-19 11:24:48 +0800

    Add some comments

--- src/replay/replayRequests.ts
+++ src/replay/replayRequests.ts
@@ -12,36 +12,40 @@ export default function recordRequests(configuration: ReplayConfig) {
     beforeEach(() => {
         logger = new Logger();
 
-        cy.readFile(
-            createFixtureFilename(
-                Cypress.config().fixturesFolder as string,
-                Cypress.spec.name,
-                Cypress.currentTest.titlePath
-            )
-        ).then(fileContents => {
-            const collection = new RequestCollection(dynamicComponentManager, logger);
-            collection.appendFromFixture(fileContents);
-            return collection;
-        }).then(requestCollection => cy.wrap<Promise<RequestCollection>, RequestCollection>(new Promise<RequestCollection>((resolve) => {
-            cy.readFile(
-                createMergedFixtureFilename(Cypress.config().fixturesFolder as string, Cypress.spec.name, Cypress.currentTest.titlePath)
-            ).should(mergeFixture => {
-                if (mergeFixture) {
-                    requestCollection.appendFromFixture(mergeFixture);
-                }
-                resolve(requestCollection);
+        // Create a request collection from the fixture fix.
+        cy.readFile(createFixtureFilename(Cypress.config().fixturesFolder as string, Cypress.spec.name, Cypress.currentTest.titlePath))
+            .then(fileContents => {
+                const collection = new RequestCollection(dynamicComponentManager, logger);
+                collection.appendFromFixture(fileContents);
+                return collection;
+            })
+
+            // Optionally append an additional hand-crafted fixture file to merge into the request collection,
+            // for cases where tests are non-deterministic or may otherwise require additional fixed requests in the
+            // same collection format.
+            .then(requestCollection => cy.wrap<Promise<RequestCollection>, RequestCollection>(new Promise<RequestCollection>((resolve) => {
+                cy.readFile(
+                    createMergedFixtureFilename(Cypress.config().fixturesFolder as string, Cypress.spec.name, Cypress.currentTest.titlePath)
+                ).should(mergeFixture => {
+                    if (mergeFixture) {
+                        requestCollection.appendFromFixture(mergeFixture);
+                    }
+                    resolve(requestCollection);
+                });
+            }), {log: false}))
+
+            // Start interception based on the resolved request collection.
+            .then(requestCollection => {
+                cy.intercept(new RegExp(configuration.interceptPattern || ".*"), (req: CyHttpMessages.IncomingHttpRequest) => {
+                    const fixtureResponse = requestCollection.shiftRequest(req);
+                    if (fixtureResponse) {
+                        req.reply({
+                            ...fixtureResponse,
+                            delay: configuration.responseDelayOverride !== undefined ? configuration.responseDelayOverride : fixtureResponse.delay,
+                        });
+                    }
+                });
             });
-        }), {log: false})).then(requestCollection => {
-            cy.intercept(new RegExp(configuration.interceptPattern || ".*"), (req: CyHttpMessages.IncomingHttpRequest) => {
-                const fixtureResponse = requestCollection.shiftRequest(req);
-                if (fixtureResponse) {
-                    req.reply({
-                        ...fixtureResponse,
-                        delay: configuration.responseDelayOverride !== undefined ? configuration.responseDelayOverride : fixtureResponse.delay,
-                    });
-                }
-            });
-        });
     });
 
     afterEach(() => {
