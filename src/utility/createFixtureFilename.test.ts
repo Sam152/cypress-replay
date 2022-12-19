6264c00953616a5e9c8362fa127429a1de58e9ea
Sam <54m152@gmail.com>
2022-12-19 08:59:17 +0800

    Add a file path generator for a merged fixture path

--- src/utility/createFixtureFilename.test.ts
+++ src/utility/createFixtureFilename.test.ts
@@ -1,8 +1,21 @@
 import {describe, expect, test} from '@jest/globals';
-import createFixtureFilename from "./createFixtureFilename";
+import createFixtureFilename, {createMergedFixtureFilename} from "./createFixtureFilename";
 
 test('that sanitized filenames are created', () => {
     expect(
         createFixtureFilename('/var/foo/bar', 'my_sample_spec.ts', ['Sample Test', 'it really works!'])
     ).toEqual('/var/foo/bar/my_sample_spec.ts/Sample-Test-it-really-works.json')
 });
+
+test('that a merged path can be created', () => {
+    expect(
+        [
+            createFixtureFilename('/path', 'spec.ts', ['spec', 'test']),
+            createMergedFixtureFilename('/path', 'spec.ts', ['spec', 'test']),
+        ]
+    ).toEqual([
+        '/path/spec.ts/spec-test.json',
+        '/path/spec.ts/spec-test.merged.json',
+    ]);
+});
+
