d39d4138d24e3243d9f98b9c1210f886611ce690
antischematic <michael.muscat@outlook.com>
2022-12-22 11:46:33 +1100

    Use Map to store incoming requests
    Handles the case where new requests are initiated while waiting for other requests to complete. These will be processed in the same waitForRequests queue.

--- src/utility/RequestCollection.ts
+++ src/utility/RequestCollection.ts
@@ -4,9 +4,7 @@ import EnvComponentManager from "./EnvComponentManager";
 import IncomingRequest = CyHttpMessages.IncomingRequest;
 import Logger, {LoggerInterface} from "./Logger";
 
-export type RequestMap = {
-    [key: string]: Promise<StaticResponse>[],
-};
+export type RequestMap = Map<string, Promise<StaticResponse>[]>
 export type RequestMapFixture = {
     [key: string]: (StaticResponse & {insertAtIndex?: number})[],
 };
@@ -22,23 +20,23 @@ export default class RequestCollection {
     constructor(envComponentManager: EnvComponentManager, logger?: LoggerInterface) {
         this.envComponentManager = envComponentManager;
         this.logger = logger || new Logger();
-        this.requests = {};
+        this.requests = new Map();
     }
 
     appendFromFixture(fixture: RequestMapFixture) {
         Object.keys(fixture).forEach(key => {
-            if (!this.requests[key]) {
-                this.requests[key] = [];
+            if (!this.requests.has(key)) {
+                this.requests.set(key, []);
             }
             fixture[key].forEach(request => {
                 // Allow requests in fixture files to specify an index where they'll be inserted. This gives
                 // some control over where manually authored fixtures are inserted, otherwise they'll be
                 // appended in the order they are encountered.
                 if (request.insertAtIndex) {
-                    this.requests[key].splice(request.insertAtIndex, 0, Promise.resolve(request));
+                    this.requests.get(key)!.splice(request.insertAtIndex, 0, Promise.resolve(request));
                 }
                 else {
-                    this.requests[key].push(Promise.resolve(request));
+                    this.requests.get(key)!.push(Promise.resolve(request));
                 }
             });
         });
@@ -46,25 +44,25 @@ export default class RequestCollection {
 
     pushIncomingRequest(request: IncomingRequest, response: Promise<StaticResponse>) {
         const key = this.envComponentManager.removeDynamicComponents(createRequestKey(request));
-        if (!this.requests[key]) {
-            this.requests[key] = [];
+        if (!this.requests.has(key)) {
+            this.requests.set(key, []);
         }
-        this.requests[key].push(response);
+        this.requests.get(key)!.push(response);
     }
 
     shiftRequest(request: IncomingRequest): Promise<StaticResponse | null> {
         const key = this.envComponentManager.removeDynamicComponents(createRequestKey(request));
-        if (!this.requests[key] || this.requests[key].length === 0) {
+        if (!this.requests.has(key) || this.requests.get(key)!.length === 0) {
             this.logger.push('Request missing from fixture', {key});
-            return Promise.resolve(null!);
+            return Promise.resolve(null);
         }
         this.logger.push('Request found in fixture', {key});
-        return Promise.resolve(this.requests[key].shift()!);
+        return Promise.resolve(this.requests.get(key)!.shift()!);
     }
 
    async waitForRequests(): Promise<ResponseMap> {
         const responses = {} as ResponseMap
-        for (const [key, response] of Object.entries(this.requests)) {
+        for (const [key, response] of this.requests) {
             responses[key] = await Promise.all(response)
         }
         return responses
